e88f555443cb386967b67684b335b2c2b503f5bc
y62 <y62@outlook.dk>
2020-10-28 04:33:53 +0100

    project start, mysql setup and testing

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mysql = require("mysql");
+
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'mydb'
+});
+
+db.connect((eer) => {
+    if (eer) throw eer;
+    console.time('mysql');
+    console.log("Database successfully connected!");
+});
+
+
+app.get("/", (req, res) => {
+    res.send("<h1>We're all gonna make it!</h1>");
+});
+
+const port = 8080;
+
+app.listen(port, () => {
+    console.log("Server is running on port:", port)
+    db.query("CREATE TABLE IF NOT EXISTS pet (name VARCHAR(20));");
+});
