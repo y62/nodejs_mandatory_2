1f1ed3ad8907bba28016aeba8010c8cc6b39a580
arch0047 <48592482+arch0047@users.noreply.github.com>
2020-11-29 00:50:45 +0100

    Added HTML pages

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = 8080;
 
 // initializing the value of rate limit variable
 const createUserLimiter =rateLimit({
-    windowMs: 2 * 60  * 1000, // 2 min.
+    windowMs: 10 * 60  * 1000, // 2 min.
     max: 3 ,     // limit each IP to 3 requests per windowMs
     message: "You have exceeded the 2 minutes limit, please come again later !"
 });
