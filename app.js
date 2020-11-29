ad8f5f7846db77e37e6f3d1a4628cbc3077dddc5
y62 <y62@outlook.dk>
2020-11-29 03:42:45 +0100

    almost done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const mysql = require('mysql');
 const session = require('express-session');
@@ -6,7 +7,7 @@ const path = require('path');
 const ejs = require('ejs');
 const bcrypt = require('bcrypt');
 const nodemailer = require('nodemailer');
-
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
@@ -17,16 +18,13 @@ app.use(express.static("frontend"));
 
 //____________________________________________________________________________
 
-/*app.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/frontend/test.html");
-});*/
 
 const connection = mysql.createConnection({
     host: 'database-2.c8e4q2gd2tmb.eu-central-1.rds.amazonaws.com',
     port: 3306,
-    user: 'admin',
-    password: '',
-    database: 'nodelogin'
+    user: process.env.DATAUSER,
+    password: process.env.DATAPASS,
+    database: process.env.DATABASE
 });
 
 connection.connect(function(error){
@@ -41,72 +39,6 @@ app.set('views',path.join(__dirname,'frontend/views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
-//ADMIN CRUD______________________________________________________________
-
-let transporter = nodemailer.createTransport({
-    service: 'outlook',
-    auth: {
-        user: process.env.EMAIL,// TODO: your gmail account
-        pass: process.env.PASSWORD // TODO: your gmail password
-    }
-});
-
-// Step 2
-let mailOptions = {
-    from: 'y62@outlook.dk', // TODO: email sender
-    to: 'yous1210@stud.kea.dk', // TODO: email receiver
-    subject: 'ANDERS LATIF',
-    text: 'HUSK AT LAVE MANDATORIES!!!!'
-};
-
-// Step 3
-transporter.sendMail(mailOptions, (err, data) => {
-    if (err) {
-        return log('Error occurs');
-    }
-    return log('Email sent!!!');
-});
-
-
-app.get('/add',(req, res) => {
-    res.render('register_user', {
-        title : 'Create an user'
-    });
-});
-
-app.post('/save',  (req, res) => {
-const username = req.body.username;
-const encryptedPassword = req.body.password;
-    let password = bcrypt.hashSync(encryptedPassword, 10);
-   // let password = hash.toString();
-    const data = {username, password};
-        let sql = "INSERT INTO users SET ?";
-        connection.query(sql, data,(err, results) => {
-        if(err) throw err;
-            transporter.sendMail(mailOptions, (err, data) => {
-                if (err) {
-                    return console.log('Error occurs');
-                }
-                return console.log('Email sent!!!');
-            });
-        res.redirect('/users');
-    });
-});
-
-app.get('/delete/:userId',(req, res) => {
-    const userId = req.params.userId;
-    let sql = `DELETE from users where id = ${userId}`;
-    let query = connection.query(sql,(err, result) => {
-        if(err) throw err;
-        res.redirect('/users');
-    });
-});
-
-app.get('/emailNotFound', (req, res) => {
-});
-
 
 //LOGIN_______________________________________________________________
 
@@ -127,16 +59,14 @@ app.get('/', function(request, response) {
 //$2b$10$8aGd9MQGHuI.iAR2BHQypeN.DdyU36KfumcjaTvL2mJ04XLWUw4L6
 
 
-
-
 app.post('/auth', function(req, res) {
-    const username = req.body.username;
+    const email = req.body.email;
     const password = req.body.password;
 
-    if (username && password) {
+    if (email && password) {
     let cuttedPass = "";
         function cutted() {
-            connection.query("SELECT password FROM users WHERE username = ?", [username, password], function (error, result, fields) {
+            connection.query("SELECT password FROM users WHERE email = ?", [email, password], function (error, result, fields) {
                 let hashedPass = JSON.stringify(result);
              //   cuttedG = hashed.substring(13, 75);
              //   console.log(cuttedG);
@@ -145,11 +75,11 @@ app.post('/auth', function(req, res) {
         }
         cutted();
 
-          connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
+          connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], function (error, results, fields) {
               let mypass = cuttedPass;
             if (bcrypt.compareSync(password, mypass) === true)  {
                 req.session.loggedin = true;
-                req.session.username = username;
+                req.session.email = email;
                 res.redirect('/users');
             } else {
                 res.send('Incorrect Username and/or Password!');
@@ -157,7 +87,7 @@ app.post('/auth', function(req, res) {
             res.end();
         });
     } else {
-        res.send('Please enter Username and Password!');
+        res.send(loginMsg);
         res.end();
     }
 });
@@ -173,18 +103,82 @@ app.get('/users',(req, res) => {
             });
         });
     } else {
-        console.log("cannot login")
+        res.send(loginMsg);
     }
 });
 
 app.get('/home', function(req, res) {
     if (req.session.loggedin) {
-        res.send('Welcome back, ' + req.session.username + '!');
+        res.send('Welcome back, ' + req.session.email + '!');
     } else {
-        res.send('Please login to view this page!');
+        res.send('Please login to view this page...');
     }
     res.end();
 });
+
+//ADMIN CRUD______________________________________________________________
+
+const loginMsg = "Please login to view this page..."
+
+function confirmationMail(confirmationAcc) {
+
+    let transporter = nodemailer.createTransport({
+        service: 'outlook',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    });
+
+    let mailOptions = {
+        from: 'y62@outlook.dk', //
+        to: confirmationAcc,
+        subject: 'Welcome ' + confirmationAcc,
+        text: 'Your account ' + confirmationAcc + ' has been successfully created!'
+    };
+
+    transporter.sendMail(mailOptions, (err, data) => {
+        if (err) {
+            return console.log('Email not sent...');
+        }
+        return console.log('Confirmation sent...');
+    });
+}
+
+
+app.get('/add', (req, res) => {
+    if (req.session.loggedin) {
+        res.render('register_user', {
+            title: 'Create an user'
+        });
+    } else {
+        res.send(loginMsg)
+    }
+});
+
+app.post('/save', createUserLimiter,  (req, res) => {
+    const email = req.body.email;
+    const encryptedPassword = req.body.password;
+    let password = bcrypt.hashSync(encryptedPassword, 10);
+    // let password = hash.toString();
+    const data = {email, password};
+    let sql = "INSERT INTO users SET ?";
+    connection.query(sql, data,(err, results) => {
+        if(err) throw err;
+        res.redirect('/users');
+        confirmationMail(email);
+    });
+});
+
+app.get('/delete/:userId',(req, res) => {
+    const userId = req.params.userId;
+    let sql = `DELETE from users where id = ${userId}`;
+    let query = connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.redirect('/users');
+    });
+});
+
 
 app.listen(port, () => {
     console.log("Server is running on port:", port)
