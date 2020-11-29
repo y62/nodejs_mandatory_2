466686cf6ecb9c3c8e7f2150752f170f0702cd14
arch0047 <48592482+arch0047@users.noreply.github.com>
2020-11-29 01:09:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const connection = mysql.createConnection({
     host: 'database-2.c8e4q2gd2tmb.eu-central-1.rds.amazonaws.com',
     port: 3306,
     user: 'admin',
-    password: 'adminadmin',
+    password: '',
     database: 'nodelogin'
 });
 
